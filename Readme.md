9fbcd26e7f134ca7ba5d224ff73014958a55225b
kazu69 <kazu69web@gmail.com>
2015-12-29 11:59:16 +0900

    hello

---
+++ Readme.md
@@ -0,0 +1,106 @@
+# domain-info
+
+Simple domain infomation client.
+
+## install
+
+```sh
+npm install domain-info
+```
+
+## Usage
+
+```js
+const domain = require('domain-info');
+const callback = (error, data) => {
+    console.log(data);
+}
+
+domain.groper('github.com', { type: ['A'] }, callback);
+// data = { A:
+//    [ { name: 'github.com',
+//        type: 1,
+//        class: 1,
+//        ttl: 20,
+//        address: '192.30.252.130' } ]
+// }
+```
+
+This module return Promise object without callback function.
+
+```js
+const domain = require('domain-info');
+
+let promise = domain.groper('github.com', { type: ['A'] });
+promise.then(data => {
+// data = { A:
+//    [ { name: 'github.com',
+//        type: 1,
+//        class: 1,
+//        ttl: 20,
+//        address: '192.30.252.130' } ]
+// }
+});
+```
+
+## API
+
+### groper(domain, [type, option, callback])
+
+Dig command.
+Option is [node-dns](https://github.com/tjfontaine/node-dns#request) Request method option.
+```type``` is resource record type like ```A```, ```MX```.
+
+```js
+const domain = require('domain-info');
+domain.groper(
+    'example.com',
+    {
+        type: ['A', 'NS', 'MX'],
+        server: { address: '8.8.8.8', port: 53, type: 'udp' },
+        timeout: 1000
+    },
+    callback
+);
+```
+
+### reverse(ip_address, [callback])
+
+Reverse DNS lookup.
+
+```js
+const domain = require('domain-info');
+domain.reverse('8.8.8.8', (error, data) => {
+    // data => [ 'google-public-dns-a.google.com' ]
+});
+```
+
+### whois(domain, [option, callback])
+Whois command.
+Option has ```server``` and ```port``` properties.
+
+```js
+const domain = require('domain-info');
+domain.whois('example.com', (error, data) => {
+    // data has whois results.
+});
+```
+
+### punycode(ascii_or_unicode)
+
+Convert ascii domain to uicode.
+Also convert unicode domain to ascii in reverse.
+
+```js
+const domain = require('domain-info');
+
+domain.punycode('日本語.jp');
+// => xn--wgv71a119e.jp
+
+domain.punycode('xn--wgv71a119e.jp');
+// => 日本語.jp
+```
+
+## License
+
+MIT © kazu69
---
+++ test/test.js
@@ -0,0 +1,186 @@
+'use strict';
+
+const test = require('ava');
+const domainInfo = require('../index');
+
+test('.groper() return promise when without callback function.', t => {
+    let domain = 'example.com',
+        type = 'A' ,
+        promise = domainInfo.groper(domain, type);
+
+    return promise.then(data => {
+        var record = data.A.find(elem => {
+            if(elem.address) return elem;
+        });
+
+         t.is(record.address, '93.184.216.34');
+    });
+});
+
+test.cb('.groper() return domain resource record.', t => {
+    const server = {
+            address: '8.8.8.8',
+            port: 53,
+            type: 'udp'
+        },
+        types = ['A', 'NS', 'MX'],
+        domain = 'example.com',
+        timeout = 3000;
+
+    const options = {
+        server: server,
+        timeout: timeout
+    }
+
+    const callback = (error, data) => {
+        var record = data.A.find(elem => {
+            if(elem.address) return elem;
+        });
+        t.is(record.address, '93.184.216.34');
+
+        var record = data.MX;
+        t.is(record.length, 0);
+
+        var ns = data.NS.find(elem => {
+            if(elem.data) return elem;
+        });
+        t.true(ns.data.indexOf('iana-servers.net') !== -1);
+        t.is(error, null);
+        t.end();
+    }
+
+    domainInfo.groper(domain, types, options, callback);
+});
+
+test('.groper() enable encode punycode domain', t => {
+    const domain = '日本語.jp',
+          type = 'A';
+
+    const callback = (error, data) => {
+
+        var record = data.A.find(elem => {
+            if(elem) return elem;
+        });
+        t.is(ecord.name, 'xn--wgv71a119e.jp');
+        t.is(record.address, '117.104.133.167');
+
+        t.is(error, null);
+    }
+
+    domainInfo.groper(domain, type, callback);
+});
+
+test('.groper() return error with message.', t => {
+    const server = {
+            address: '0.0.0.0',
+            port: 53,
+            type: 'udp'
+        },
+        types = ['A', 'CNAME', 'MX'],
+        domain = 'example.com',
+        timeout = 3000;
+
+    const options = {
+        server: server,
+        timeout: timeout
+    }
+
+    const callback = (error, data) => {
+        t.is(error[0], 'Error: DNS request timed out');
+    }
+
+    domainInfo.groper(domain, types, options, callback);
+});
+
+test('.groper() throw error when supply invalid arguents', t => {
+    let domain = null,
+        options = {},
+        callback = (error, data) => {};
+    t.throws(() => { domainInfo.groper(domain, callback) }, 'Expected a `domain`');
+
+    domain = 'example.com';
+    t.throws(() => { domainInfo.groper(domain, [], options, callback) }, 'Valid No `DNS TYPE` exitst');
+
+    options = { type: null }
+    t.throws(() => { domainInfo.groper(domain, null, options, callback) }, 'Expected a `types is Array`');
+});
+
+test('.reverse() return promise when without callback function.', t => {
+    const ip = '8.8.8.8',
+        promise = domainInfo.reverse(ip);
+
+    return promise.then(hostname => {
+        t.same(hostname, ['google-public-dns-a.google.com']);
+    });
+});
+
+test.cb('.reverse() return hostname.', t => {
+    const ip = '8.8.8.8',
+        callback = (error, data) => {
+            t.same(data, ['google-public-dns-a.google.com']);
+            t.end();
+        };
+
+    domainInfo.reverse(ip, callback);
+});
+
+test('.reverse() throw error when supply invalid arguents', t => {
+    const ip = null;
+    t.throws(() => { domainInfo.reverse(ip) }, 'Expected a `ip address`');
+});
+
+test('.whois() return promise when without callback function.', t => {
+    const domain = 'example.com',
+        options = {
+            server: 'whois.verisign-grs.com',
+            port: 43
+        },
+        promise = domainInfo.whois(domain, options);
+
+    return promise.then(data => {
+        t.not(data, null);
+        t.not(data, undefined);
+        t.not(data, '');
+        t.true(typeof data === 'string');
+    });
+});
+
+
+test.cb('.whois() return domain information', t => {
+    const domain = 'example.com',
+        options = {
+            server: 'whois.verisign-grs.com',
+            port: 43
+        },
+        callback = (error, data) => {
+            t.not(data, null);
+            t.not(data, undefined);
+            t.not(data, '');
+            t.true(typeof data === 'string');
+            t.end();
+        };
+
+    domainInfo.whois(domain, options, callback);
+});
+
+test('.whois() throw error when supply invalid arguents', t => {
+    let domain = null;
+    t.throws(() => { domainInfo.whois(domain) }, 'Expected a `domain`');
+
+    domain = 'foobar.dummy';
+    t.throws(() => { domainInfo.whois(domain) }, 'Invalid a `domain name`');
+});
+
+test('.punycode() return encoded string when supply unicode', t => {
+    const domain = '日本語.jp',
+          encoded = domainInfo.punycode(domain);
+
+    t.is(encoded, 'xn--wgv71a119e.jp');
+});
+
+test('.punycode() return decoded string when supply ascii', t => {
+    const domain = 'xn--wgv71a119e.jp',
+          encoded = domainInfo.punycode(domain);
+
+    t.is(encoded, '日本語.jp');
+});
